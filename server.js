928ceadcba6fde25885085dcedc0f2241590e552
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-05-29 22:15:53 +0700

    Route Parameters

--- server.js
+++ server.js
@@ -4,11 +4,36 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send({
+const friends = [
+  {
     id: 1,
-    name: "Trieu Ngoc Hai"
-  });
+    name: "Quentin Tarantino",
+  },
+  {
+    id: 2,
+    name: "Christopher Nolan",
+  },
+  {
+    id: 3,
+    name: "Clint Eastwood",
+  },
+];
+
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist.",
+    });
+  }
 });
 
 app.get("/messages", (req, res) => {
